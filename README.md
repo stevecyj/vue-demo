ad8d26ced20e7450c9883016b8905bc6a4302258
Steve <ycsteven.tsao@gmail.com>
2020-08-11 07:34:39 +0800

    1st commit

--- README.md
+++ README.md
@@ -1,24 +1,31 @@
 # vue_demo_project
 
+## 測試 style、語法、套件
+
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+// Import component
+import Loading from 'vue-loading-overlay';
+// Import stylesheet
+import 'vue-loading-overlay/dist/vue-loading.css';
+
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
+// 元件，全域註冊
+Vue.component('Loading', Loading);
+Vue.use(VueAxios, axios);
 
 new Vue({
   router,
