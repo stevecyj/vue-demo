04b3feb6d6070d9958e482ed0b54dd225f9e9f99
Steve <ycsteven.tsao@gmail.com>
2020-08-24 07:48:33 +0800

    login complete

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h2>這裡是管理頁面</h2>
-    <router-view />
+    <router-view :token="token" v-if="checkSuccess" />
   </div>
 </template>
 
@@ -13,13 +13,30 @@ export default {
       checkSuccess: false,
     };
   },
-  created() {},
+  created() {
+    this.checkLogin();
+  },
   methods: {
     checkLogin() {
       // 取得 token 的 cookies（注意取得的時間點）
       // 詳情請見：https://developer.mozilla.org/zh-CN/docs/Web/API/Document/cookie
       this.token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
       this.$http.defaults.headers.common.Authorization = `Bearer ${this.token}`;
+
+      const api = `${process.env.VUE_APP_APIPATH}api/auth/check`;
+      this.$http
+        .post(api, {
+          api_token: this.token,
+        })
+        .then((res) => {
+          console.log(res);
+          this.checkSuccess = true;
+        })
+        .catch((err) => {
+          console.log(err.response);
+          // 驗證失敗時，回到登錄頁
+          this.$router.push('/login');
+        });
     },
   },
 };
--- src/views/dashboard/Products.vue
+++ src/views/dashboard/Products.vue
@@ -16,15 +16,14 @@ export default {
   data() {
     return {
       products: [],
-      token: '',
     };
   },
+  props: ['token'],
   created() {
     // 取得 token 的 cookies（注意取得的時間點）
     // 詳情請見：https://developer.mozilla.org/zh-CN/docs/Web/API/Document/cookie
-    this.token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
-    this.$http.defaults.headers.common.Authorization = `Bearer ${this.token}`;
-
+    // this.token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+    // this.$http.defaults.headers.common.Authorization = `Bearer ${this.token}`;
     // 後台管理 api
     const url = `${process.env.VUE_APP_APIPATH}api/${process.env.VUE_APP_UUID}/admin/ec/products`;
     this.$http.get(url).then((res) => {
